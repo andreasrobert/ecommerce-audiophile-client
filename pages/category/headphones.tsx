91a77599fc3913738d395ae65b4e0cbd9d1c6ea4
andreasrobert <andreasrobb23@gmail.com>
2021-08-08 21:05:32 +0700

    graphql fix

--- pages/category/headphones.tsx
+++ pages/category/headphones.tsx
@@ -1,39 +1,53 @@
 import Navbar from "../../components/navbar";
 import Title from "../../components/title";
-import Onload from "../../components/onload"
+import Onload from "../../components/onload";
 import Category from "../../components/category";
 import Footer from "../../components/footer";
 import Product from "../../components/product";
 import WhatIsAudiophile from "../../components/whatIsAudiophile";
-import { useQuery } from "@apollo/client";
-import { LOAD_FOR_CATEGORY } from '../../graphql/queries';
-  
-const Headphones = () =>{
-    const { loading, error, data, refetch } = useQuery(LOAD_FOR_CATEGORY,{ variables: {Category: "headphones"}});
+import { useLazyQuery } from "@apollo/client";
+import { LOAD_FOR_CATEGORY } from "../../graphql/queries";
+import { useEffect } from "react";
 
-    if (error) {
-        if (error.message.toLocaleLowerCase().includes('timeout')) {
-            refetch();
-        }else {return `Error! ${error}`;}
-        
+const Headphones = () => {
+  const [loadCat, { loading, error, data }] = useLazyQuery(
+    LOAD_FOR_CATEGORY,
+    { variables: { Category: "headphones" } },
+  );
+
+  useEffect(() => {
+    if (error || !loading || !data) {
+      loadCat({ variables: { Category: "headphones" } });
     }
-    var order = 0; 
+  }, [error, loading, data, loadCat]);
 
 
-    return(
-        <>
-                <Navbar></Navbar>
-                <Title title="HEADPHONES"></Title>
-                
-                {loading ? <Onload></Onload> : data?.productsbyCategory.map((product:any)=>{
-                    order = order+1;
-                    return <Product key={product._id} data={product} order={order}></Product>
-                })}
-                <Category></Category>
-                <WhatIsAudiophile></WhatIsAudiophile>
-                <Footer></Footer>
-        </>
-    );
-}
+  if (error) {
+    if (error.message.toLocaleLowerCase().includes("timeout")) {
+    } else {
+      return `Error! ${error}`;
+    }
+  }
+  var order = 0;
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Title title="HEADPHONES"></Title>
+
+      {loading ? (
+        <Onload></Onload>
+      ) : (
+        data?.productsbyCategory.map((product: any) => {
+          order = order + 1;
+          return <Product key={product._id} data={product} order={order}></Product>;
+        })
+      )}
+      <Category></Category>
+      <WhatIsAudiophile></WhatIsAudiophile>
+      <Footer></Footer>
+    </>
+  );
+};
 
 export default Headphones;
--- pages/category/speakers.tsx
+++ pages/category/speakers.tsx
@@ -1,28 +1,45 @@
 import Navbar from "../../components/navbar";
 import Title from "../../components/title";
+import Onload from "../../components/onload";
 import Category from "../../components/category";
 import Footer from "../../components/footer";
 import Product from "../../components/product";
 import WhatIsAudiophile from "../../components/whatIsAudiophile";
 
-import { useQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import { LOAD_FOR_CATEGORY } from '../../graphql/queries';
+import { useEffect } from "react";
   
 
 const Speakers = () =>{
-    const { loading, error, data } = useQuery(LOAD_FOR_CATEGORY,{ variables: {Category: "speakers"}});
-    if (loading) return null;
-    if (error) return `Error! ${error}`;
-    var order = 0; 
+    const [loadCat, { loading, error, data }] = useLazyQuery(
+        LOAD_FOR_CATEGORY,
+        { variables: { Category: "speakers" } },
+      );
+    
+      useEffect(() => {
+        if (error || !loading || !data) {
+          loadCat({ variables: { Category: "speakers" } });
+        }
+      }, [error, loading, data, loadCat]);
+    
+      if (error) {
+        if (error.message.toLocaleLowerCase().includes("timeout")) {
+        } else {
+          return `Error! ${error}`;
+        }
+      }
+      var order = 0;
 
     return(
         <>
             <Navbar></Navbar>
             <Title title="SPEAKERS"></Title>
-            {data.productsbyCategory.map((product:any)=>{
+            {loading?(<Onload></Onload>) : (data?.productsbyCategory.map((product:any)=>{
                     order = order+1;
                     return <Product key={product._id} data={product} order={order}></Product>
-                })}
+                })
+                )}
             <Category></Category>
             <WhatIsAudiophile></WhatIsAudiophile>
             <Footer></Footer>
