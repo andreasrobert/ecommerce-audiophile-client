0c002a0f418c455d639b222c89356966a6b34476
andreasrobert <andreasrobb23@gmail.com>
2021-12-18 19:11:19 +0700

    styled component onload fix1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react"
+import Head from "next/head";
 import { theme } from '../styles/theme'
 import { client } from '../graphql/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <ChakraProvider theme={theme}>
+     <Head>
+        <title>Audiophile</title>
+      </Head>
   <ApolloProvider client={client}>
   <Component {...pageProps} />
   </ApolloProvider>
